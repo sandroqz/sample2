e375e88cb8e21a0d70467d70e8f94506aeb947cb
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-01-28 04:41:10 -0300

    Mapeamento de métodos personalizados em recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Sample2::Application.routes.draw do
+  constraints(lambda { |req| req.env["HTTP_USER_AGENT"] =~ /iPhone/ }) do
+    root to: "users#index"
+  end
   root to: "users#index"
   resource :sessions, only: [:new, :create, :destroy]
-  scope shallow_path: "usr" do
-    resources :users, except: [:show] do
-      resources :addresses, shallow: true, path_names: {new: "new_form", edit: "edit_form"}
+  resources :users, except: [:show] do
+    member do
+      put 'activate'
     end
+    collection do
+      get 'active_users'
+    end
+    resources :addresses, path_names: {new: "new_form", edit: "edit_form"}
   end
 end
