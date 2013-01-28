f7175529cf074e4e2faf43f1c56889ef89c9caf8
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-01-28 03:32:54 -0300

    Personalizando o mapeamento de recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Sample2::Application.routes.draw do
   root to: "users#index"
-  resource :sessions
-  resources :users do
-    resources :addresses
+  resource :sessions, only: [:new, :create, :destroy]
+  scope shallow_path: "usr" do
+    resources :users, except: [:show] do
+      resources :addresses, shallow: true, path_names: {new: "new_form", edit: "edit_form"}
+    end
   end
 end
